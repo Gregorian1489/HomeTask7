25a9f77473d05f5e3a4fac3ac5f34fa059d42049
Gregorian1489 <santijago@mail.ru>
2022-12-03 18:51:24 +0400

    add ht47

---
+++ ht47/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Введите количество строк массива");
+int line = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива");
+int col = Convert.ToInt32(Console.ReadLine());
+
+double[,]array = new double[line,col];
+Random rand = new Random();
+
+for(int i = 0;i<line;i++)
+{
+    for(int j = 0; j<col;j++)
+    {
+        array[i,j] = rand.Next(-100, 100) + rand.NextDouble();
+        Console.Write("{0,7}",Math.Round(array[i,j],2));
+    }
+    Console.WriteLine();
+}
