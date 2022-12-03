a7e16e1ddabd81cfc66ee2830feacc3788ecce2b
Gregorian1489 <santijago@mail.ru>
2022-12-03 20:43:46 +0400

    add ht50

---
+++ ht52/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.WriteLine("Введите количество строк массива");
+int line = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива");
+int col = Convert.ToInt32(Console.ReadLine());
+int[,]array = new int[line,col];
+double sum = 0;
+double result = 0;
+
+for(int i = 0;i<line;i++)
+{
+    for(int j = 0; j<col;j++)
+    {
+        array[i,j] = new Random().Next(1,10);
+        Console.Write(array[i,j]+" ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+Console.Write("Среднее арифметическое каждого столбца: ");
+for(int j = 0;j<col;j++)
+{
+    for(int i = 0; i<line;i++)
+    {
+        sum = sum + array[i,j];
+        result = sum/col;
+    }
+    Console.Write(Math.Round(result,2)+" | ");
+    sum = 0;
+}
+
